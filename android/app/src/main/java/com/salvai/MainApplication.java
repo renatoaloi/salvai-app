4de2564bb06eb78c1abb93a24eda4f46358075b5
Renato Aloi <renato.aloi@gmail.com>
2018-11-09 00:47:20 -0200

    First run

--- android/app/src/main/java/com/salvai/MainApplication.java
+++ android/app/src/main/java/com/salvai/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
-import com.imagepicker.ImagePickerPackage;
 import com.reactlibrary.RNReactNativeSelectAllImagesPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -27,7 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new PickerPackage(),
-            new ImagePickerPackage(),
             new RNReactNativeSelectAllImagesPackage()
       );
     }
